c5925e0d47d2b85d1ca086f6c69f3ffe34c1d69a
Andy <enquiries.alee@protonmail.com>
2022-03-11 17:00:02 +0000

    Initialised Svelte

---
+++ client/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Layout from './__layout.svelte';
+</script>
+
+<div class="container mx-auto mt-10">
+	<!-- <h1>Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+	<h1 class="text-3xl font-bold underline mt-10">Movies</h1>
+</div>
