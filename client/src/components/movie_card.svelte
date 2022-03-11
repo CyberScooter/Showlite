d0bb599b9d0ced7a7c5ac57719cd1bf9e4a52ce6
Andy <enquiries.alee@protonmail.com>
2022-03-11 23:04:55 +0000

    Added carousel

---
+++ client/src/components/movie_card.svelte
@@ -0,0 +1,34 @@
+<!-- <div class="w-30 rounded overflow-hidden shadow-lg">
+    <img class="w-full h-80 bg-gray-200" src="static/placeholder-image.png" alt="Sunset in the mountains">
+    <div class="px-6 py-4 ">
+      <div class="font-bold text-xl mb-2">Movie Title</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    </div>
+  </div> -->
+
+  <div class="flex flex-shrink-0 relative w-full sm:w-auto">
+    <img
+        src="static/film-poster-placeholder.png"
+        alt="Movie Cover"
+        class="object-cover object-center w-96 h-full"
+    />
+    <div class="bg-gray-800 bg-opacity-30 absolute w-96 h-full p-6">
+        <h2 class="lg:text-xl leading-4 text-base lg:leading-5 text-white dark:text-gray-900">
+            Movie #
+        </h2>
+        <div class="flex h-full items-end pb-6">
+            <h3
+                class="text-xl lg:text-2xl font-semibold leading-5 lg:leading-6 text-white dark:text-gray-900"
+            >
+                Movie Title
+            </h3>
+        </div>
+    </div>
+</div>
